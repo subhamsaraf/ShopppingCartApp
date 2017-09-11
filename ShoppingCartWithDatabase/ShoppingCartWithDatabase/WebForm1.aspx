4643151b0ee9990aca2f6c3f162447217d287526
ssaraf <ssaraf@tavisca.com>
2017-09-12 00:03:10 +0530

    Linked the Database with The Main Screen

---
+++ ShoppingCartWithDatabase/ShoppingCartWithDatabase/WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ShoppingCartWithDatabase.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:GridView ID="GridView1" runat="server" OnSelectedIndexChanged="AddToCart">
+            <Columns>
+                <asp:TemplateField HeaderText="Add To Cart">
+                    <ItemTemplate>
+                        <asp:Button ID="Button2" runat="server" Text="Add To Cart" />
+                    </ItemTemplate>
+                </asp:TemplateField>
+            </Columns>
+        </asp:GridView>
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="View Cart" />
+    </form>
+</body>
+</html>
---
+++ ShoppingCartWithDatabase/ShoppingCartWithDatabase/WebForm1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace ShoppingCartWithDatabase
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connectionString = WebConfigurationManager.ConnectionStrings["ShoppingProductsDB"].ConnectionString;
+            string command = "Select * from Product";
+            GridView1.DataSource = getDataItem(connectionString, command).Tables[0];
+            GridView1.DataBind();
+        }
+        private DataSet getDataItem(string connectionstring, string command)
+        {
+            SqlConnection con = new SqlConnection(connectionstring);
+            SqlCommand comand = new SqlCommand(command, con);
+            SqlDataAdapter adapter = new SqlDataAdapter(comand);
+            DataSet dataset = new DataSet();
+            adapter.Fill(dataset);
+            return dataset;
+        }
+        protected void AddToCart(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Cart.aspx");
+        }
+    }
+}
